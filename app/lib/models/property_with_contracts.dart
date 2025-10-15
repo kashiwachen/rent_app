aedfad97ee9862a99cf785194431fd4b68ffb3dc
kashiwachen <pochiachen0321@gmail.com>
2025-10-15 18:49:26 +0900

    feat: add data model for property

--- app/lib/models/property_with_contracts.dart
+++ app/lib/models/property_with_contracts.dart
@@ -1,36 +1,43 @@
-/// Model for property with its associated contracts
+/// Model class that combines a Property with its associated Contracts
 ///
-/// Used by PropertyRepository.getPropertiesWithContracts() to return
-/// a property along with all its contracts (both active and inactive)
+/// This model is used for business logic that needs to work with
+/// properties and their rental history together.
 library;
 
 import '../core/database/app_database.dart';
 
-/// Represents a property with all its contracts
+/// A property with all its associated contracts
 class PropertyWithContracts {
   /// The property entity
   final Property property;
 
   /// List of contracts associated with this property
+  /// Empty list if property has no contracts
   final List<Contract> contracts;
 
-  const PropertyWithContracts({
-    required this.property,
-    required this.contracts,
-  });
+  PropertyWithContracts({required this.property, required this.contracts});
 
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is PropertyWithContracts &&
-          runtimeType == other.runtimeType &&
-          property.id == other.property.id;
-
-  @override
-  int get hashCode => property.id.hashCode;
-
-  @override
-  String toString() {
-    return 'PropertyWithContracts{property: ${property.name}, contractCount: ${contracts.length}}';
+  /// Returns the currently active contract for this property, if any
+  Contract? get activeContract {
+    try {
+      return contracts.firstWhere((c) => c.isActive);
+    } catch (_) {
+      return null;
+    }
   }
+
+  /// Whether this property currently has an active contract
+  bool get isOccupied => activeContract != null;
+
+  /// Whether this property is currently vacant
+  bool get isVacant => !isOccupied;
+
+  /// Total number of contracts this property has had
+  int get totalContracts => contracts.length;
+
+  /// Number of active contracts for this property
+  int get activeContractCount => contracts.where((c) => c.isActive).length;
+
+  /// Number of inactive/expired contracts for this property
+  int get inactiveContractCount => contracts.where((c) => !c.isActive).length;
 }
